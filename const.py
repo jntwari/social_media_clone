da98246d07b8676fd01e66935b0894ed18f5c3fc
Jean Bosco Ntwari <73104162+jntwari@users.noreply.github.com>
2021-08-25 17:44:40 +0200

    Delete const.py

--- const.py
+++
@@ -1,4 +0,0 @@
-SECRET_KEY = 'MMKJIYTIOMLKHSHSHSKKSSSSS_+&^^*%%#$KKLLBSBSSBSBSL:$'
-DB = 'postgresql://postgres:postgres@localhost/clone'
-# DB2 = 'mysql://username:password@server/db'
-# DB = 'mysql://sql11431699:Amx96wc7Lm@sql11.freemysqlhosting.net/sql11431699'
